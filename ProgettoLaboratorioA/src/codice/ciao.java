b2b1fa8a5cb4ccd154bd8c74fdbaca7397f0476e
rameee01 <104573341+rameee01@users.noreply.github.com>
2022-04-28 17:02:44 +0200

    ho modificato una roba

--- ProgettoLaboratorioA/src/codice/ciao.java
+++ ProgettoLaboratorioA/src/codice/ciao.java
@@ -5,6 +5,10 @@ public class ciao {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("CIOA");
+		int ciao = 0;
+		for(int i = 0;i<10;i++) {
+			ciao +=i;
+		}
 	}
 
 }
