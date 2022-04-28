f62122abecf29e866fad6d87af9522121ae0ce3d
mirkopuglisidev <76923557+mirkopuglisidev@users.noreply.github.com>
2022-04-28 17:34:48 +0200

    :DDD

--- ProgettoLaboratorioA/src/codice/bella.java
+++ ProgettoLaboratorioA/src/codice/bella.java
@@ -4,7 +4,7 @@ public class bella {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("dio bestia");
-		System.out.println("madonna magnetica nella valle dei cazzi");
+		System.out.println("madonna magnetica nella valle dei cazzi di metallo");
 		
 	}
 }
