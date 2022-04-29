0cb74e0164dabc190f2af248ea3188e7fff76297
mirkopuglisidev <76923557+mirkopuglisidev@users.noreply.github.com>
2022-04-29 09:14:54 +0200

    heyyyyy

---
+++ ProgettoLaboratorioA/src/codice/classePrincipale.java
@@ -0,0 +1,10 @@
+package codice;
+
+public class classePrincipale {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("dio bestia");
+		System.out.println("viva Gesu bambino"); // modificato
+		
+	}
+}
