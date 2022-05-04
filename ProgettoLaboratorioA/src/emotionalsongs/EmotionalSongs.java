0770972aa9c59c580ef880ab0933fa74556c4762
rameee01 <104573341+rameee01@users.noreply.github.com>
2022-05-04 20:09:22 +0200

    Update al volo

--- ProgettoLaboratorioA/src/emotionalsongs/EmotionalSongs.java
+++ ProgettoLaboratorioA/src/emotionalsongs/EmotionalSongs.java
@@ -3,26 +3,26 @@ import java.io.IOException;
 import java.util.*;
 public class EmotionalSongs {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) { //main del progetto
 		
 		Utente u = new Utente();
 		
 		int c = 0;
 		Scanner sc = new Scanner(System.in);
 		
-		do {
-			String[] utente = u.loggedUser;
-			if(utente!=null&&!utente[0].equals("")&&utente[1].equals("")) {
-				System.out.println("Loggato come: "+utente[0]);
+		do { //Ciclo do while per lo switch (da rivedere)
+			String[] utente = u.loggedUser; //prende le informazioni dell'utente loggato
+			if(utente!=null&&!utente[0].equals("")&&!utente[1].equals("")) { //se la stringa non è vuota significa che qualcuno è loggato
+				System.out.println("Loggato come: "+utente[0]); //e nel caso stampa il nome dell'utente
 			}
 			System.out.println("Cosa vuoi fare?\n1.Registrarti\n2.Login");
 			int scelta = sc.nextInt();
 			switch(scelta) {
 			case 1:
-				u.Registrazione();
+				u.Registrazione(); //scelta di registrazione 
 				break;
 			case 2:
-				u.Login();
+				u.Login(); //scelta di login
 				break;
 			}
 			System.out.println("CONTINUARE?\n1.SI\n2.NO");
