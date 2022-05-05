3a7396d4849b256f270df0f92e922d9d58ea7e9c
rameee01 <104573341+rameee01@users.noreply.github.com>
2022-05-05 13:59:48 +0200

    Update Utente.java

--- ProgettoLaboratorioA/src/emotionalsongs/Utente.java
+++ ProgettoLaboratorioA/src/emotionalsongs/Utente.java
@@ -1,4 +1,4 @@
-package EmotionalSongs;
+package emotionalsongs;
 import java.io.*;
 import java.util.*;
 
@@ -12,11 +12,14 @@ public class Utente {
 	private String passwd;
 	
 	
-	public String[] loggedUser = {"",""}; //vettore dove salvare username e password dell'utente loggato
+	public String[] loggedUser = {"",""};
 	
-	public Utente() {}
+	public Utente() {
+		PrintWriter pw = fileManager("session.txt", false);
+		pw.print("");
+	}
 	
-	public Utente(String nome, String cognome, String codFisc, String ind, String mail, String username, String passwd) { //costruttore se serve
+	public Utente(String nome, String cognome, String codFisc, String ind, String mail, String username, String passwd) {
 		this.nome = nome;
 		this.cognome = cognome;
 		this.codFisc = codFisc;
@@ -26,85 +29,118 @@ public class Utente {
 		this.passwd = passwd;
 	}
 	
-	public void Registrazione() { //metodo per la registrazione
+	
+	public PrintWriter fileManager(String file, boolean append) {
+		PrintWriter pw = null;
+		try {
+			FileWriter fw = new FileWriter(file, append);
+			BufferedWriter bw = new BufferedWriter(fw);
+			pw = new PrintWriter(bw);
+			
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+		return pw;
+	}
+	
+	public void sessionManager(String[] loggeduser) {
+		
+		String file = "session.txt"; 
+		PrintWriter pw = fileManager(file, false);
+		pw.print(loggeduser[0]+"\n"+loggeduser[1]);
+		pw.close();
+	}
+	
+	public boolean checkMail(String mail) {
+		boolean check_at = false;
+		boolean check_dot = false;
+		String[] parti;
+		for(int i=0;i<mail.length();i++) {
+			if(mail.charAt(i)=='@'||check_at) {
+				check_at = true;
+			}
+		}
+		if(check_at) {
+			parti = mail.split("@");
+			for(int i=0;i<parti[1].length();i++) {
+				if(parti[1].charAt(i)=='.'||check_dot) {
+					check_dot = true;
+				}
+			}
+		}
+			
+		
+		
+		return check_at&&check_dot;
+	}
+	public void Registrazione() {
 		Scanner sc = new Scanner(System.in);
 		//creazioneFileUtente();
-		
-		//lettura dei vari campi
 		System.out.print("Nome:"); this.nome = sc.next();
 		System.out.print("Cognome:"); this.cognome = sc.next();
 		System.out.print("Codice fiscale:"); this.codFisc = sc.next();
-		System.out.print("Indirizzo fisico:"); this.ind = sc.next();
+		System.out.print("Indirizzo fisico:"); this.ind = sc.nextLine();
 		System.out.print("Mail:"); this.mail = sc.next();
-		System.out.print("Username:"); this.username = sc.next();
-		System.out.print("Password:"); this.passwd = sc.next();
-		try {//try catch per l'apertura del file
-			FileWriter fw = new FileWriter("UtentiRegistrati.csv", true); //apre il file csv con l'opzione di accodare i nuovi inserimenti
-			BufferedWriter bw = new BufferedWriter(fw);
-			PrintWriter pw = new PrintWriter(bw);
-			pw.println(nome+","+cognome+","+codFisc+","+ind+","+mail+","+username+","+passwd); //stampa nel file la riga con i vari campi
-			pw.flush();
-			pw.close();
-			sc.close();
-			//CIAO COME VA
-		}catch(IOException e) { //eccezione nel caso di errore
-			System.out.println("Errore di scrittura");
-			e.printStackTrace();
-			
+		while(!checkMail(mail)) {
+			System.out.print("Inserire una mail valida:");
+			this.mail = sc.next();
 		}
 		
+		System.out.print("Username:"); this.username = sc.next();
+		System.out.print("Password:"); this.passwd = sc.next();
 		
-		
+		String file = "UtentiRegistrati.csv";	
+		PrintWriter pw = fileManager(file, true);
+		pw.println(nome+","+cognome+","+codFisc+","+ind+","+mail+","+username+","+passwd);
+		pw.flush();
+		pw.close();
+		sc.close();
+			
+
 	}
 	
-	public void Login() { //metodo per il login
+	public void Login() {
 		String user;
 		String passwd;
 		
 		String r_user = "", r_pass = "";
 		Scanner sc = new Scanner(System.in);
-		//lettura di username e password dell'utente
 		System.out.print("Username: "); user = sc.next();
 		System.out.print("Password: "); passwd = sc.next();
-//N I C E
-		try {//try catch per l'apertura del file
+		
+		try {
 			
 			FileReader fr = new FileReader(new File("UtentiRegistrati.csv"));
 			BufferedReader br = new BufferedReader(fr);
 			String line;
-			String[] users; //stringa per splittare i campi delle linee del file
-			boolean check = false; //booleano per verificare il login
-			while((line = br.readLine())!= null&&!check) { 
+			String[] users;
+			boolean check = false;
+			while((line = br.readLine())!= null&&!check) {
 				users = line.split(",");
-				//r_user e r_pass prendono i valori presi dalla linea del file e li mette tutti in minuscolo (da sistemare ancora)
-				//la posizione 5 e 6 del vettore users corrisponde al campo username e password della linea del file (quinto e sesto campo del file)
 				r_user = users[5].toLowerCase();
 				r_pass = users[6].toLowerCase();
-				
-				if(user.equals(r_user)&&passwd.equals(r_pass)) {//verifica che i valori immessi dall'utente e quelli letti dal file siano uguali
-					check = true; //se è vero allora il login è stato eseguito correttamente
+				if(user.equals(r_user)&&passwd.equals(r_pass)) {
+					check = true;
 				}
 				
 			}
-			if(check) {//se il login è stato eseguito correttamente i dati immessi vengono salvati nel vettore nelle prime due posizioni
-				loggedUser[0] = user; 
+			if(check) {
+				loggedUser[0] = user;
 				loggedUser[1] = passwd;
+				sessionManager(loggedUser);
 				System.out.println("Loggato correttamente");
 			}	
-			else { //altrimenti è da rifare  (da sistemare)
+			else {
 				System.out.println("Username o password non corretti");
 				return;
 			}
-		} catch (FileNotFoundException e) { //vari catch
+		} catch (FileNotFoundException e) {
 			
 			e.printStackTrace();
 		} catch (IOException e) {
 		
 			e.printStackTrace();
 		}
-			
-			
-		
-		
+	
 	}
 }
