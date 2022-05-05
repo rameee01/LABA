0e3871ef70aed66ef9bdd4c6cdaedf2d6abf2a34
FeMaspe02 <femaspe02@gmail.com>
2022-05-05 18:17:25 +0200

    modifiche

--- ProgettoLaboratorioA/src/emotionalsongs/Playlist_utente.java
+++ ProgettoLaboratorioA/src/emotionalsongs/Playlist_utente.java
@@ -1,8 +1,15 @@
-public class Playlist_utente {
+package emotionalsongs;
 
-	public static void main(String[] args) {
-		
-	}
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Playlist_utente {
+	
 	
 	
 }
